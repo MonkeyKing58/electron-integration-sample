bd7b322fb1f8cadd8d665e62b99c7a6832fc9386
Max Troshin <tmv@tomsksoft.com>
2023-08-18 18:25:06 +0700

    enable nodeIntegration for loading native module in preload

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const loadMainWindow = () => {
         width : 1280,
         height: 720,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true
         }
     });
 
