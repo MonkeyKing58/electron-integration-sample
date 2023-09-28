8cdffe88da48d97c1c26af6844f013fe08a54fc6
Konstantin S. Martynov <marty68953@mail.ru>
2023-09-28 18:02:38 +0300

    Set sdk config

--- preload.js
+++ preload.js
@@ -1,21 +1,26 @@
-const { contextBridge, ipcRenderer } = require('electron')
+const { contextBridge, ipcRenderer } = require("electron");
 const tsvb = require("./libs/win/tsvb");
+const path = require("path");
+
 //const tsvb = require("./libs/linux/TSVB");
 //const tsvb = require("./libs/mac/tsvb");
 
 const inference = new tsvb.InferenceSession();
 
-contextBridge.exposeInMainWorld('inferer', {
-     run: (data) => {
-          return inference.run(data);
-     },
-     configure: (data) => {
-          return inference.configure(data);
-     },
-     auth: (data) => {
-          return inference.auth(data);
-     },
-     setPreset: (preset) => {
-          inference.setPreset(preset);
-     }
+// Define window.root_path
+contextBridge.exposeInMainWorld("root_path", path.resolve(__dirname, ""));
+
+contextBridge.exposeInMainWorld("inferer", {
+  run: (data) => {
+    return inference.run(data);
+  },
+  configure: (data) => {
+    return inference.configure(data);
+  },
+  auth: (data) => {
+    return inference.auth(data);
+  },
+  setPreset: (preset) => {
+    inference.setPreset(preset);
+  },
 });
